45fbcaad394e7142c8f3d4419279bed6249e81d3
unknown <machajagadish456@gmail.com>
2021-07-22 15:10:37 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+function change(){
+    document.getElementById("I1").innerHTML="This is my <br> mini-project";
+    document.getElementById("I1").style.color="yellow";
+}
+function change_Bg(){
+    document.body.style.backgroundImage="url('fback.jpg')";
+}
+function Url(){
+    window.open("https://www.google.com");
+}
+function sign(){
+    window.open("https://accounts.google.co.in/");
+}
+function fb(){
+    window.open("https:www.facebook.com");
+}
+function ig(){
+    window.open("https://www.instagram.com");
+}
+function tw(){
+    window.open("https://www.twitter.com")
+}
