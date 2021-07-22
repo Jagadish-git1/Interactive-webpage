3969488e28ef1507683d4eef5e336f55b5692cc2
Jagadish-git1 <87804732+Jagadish-git1@users.noreply.github.com>
2021-07-22 16:09:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
+var s=1;
+var c=1;
 function change(){
-    document.getElementById("I1").innerHTML="This is my <br> mini-project";
-    document.getElementById("I1").style.color="yellow";
+    if(c===1){
+        document.getElementById("I1").innerHTML="This is my <br> mini-project";
+        document.getElementById("I1").style.color="yellow";
+        c=2;}
+    else{
+        document.getElementById("I1").innerHTML="This is my <br> webpage!";
+        document.getElementById("I1").style.color="white";
+        c=1;}
 }
 function change_Bg(){
+    if(s===1){
     document.body.style.backgroundImage="url('fback.jpg')";
+    s=2;
+    }
+    else{
+        document.body.style.backgroundImage="url('background.png')";
+        s=1;
+    }
 }
 function Url(){
     window.open("https://www.google.com");
@@ -12,7 +27,7 @@ function sign(){
     window.open("https://accounts.google.co.in/");
 }
 function fb(){
-    window.open("https://www.facebook.com/");
+    window.open("https:www.facebook.com");
 }
 function ig(){
     window.open("https://www.instagram.com");
